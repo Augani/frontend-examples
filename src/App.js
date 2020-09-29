355c692edd52f51c274aed73dd455b0c87fd8fe4
Augustus Otu <Augani7@gmail.com>
2020-09-29 18:48:22 +0100

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
 import "./style.css";
+import "./css/tailwind.css"
+import Bank from './pages/bank'
 
 export default function App() {
+  const[index, setIndex] = React.useState(0)
+  const page = (v)=>{
+      switch(v){
+        case 0:
+          return <Main navigate={(e)=>setIndex(e)}/>;
+          case 1:
+            return <Bank/>;
+        default:
+          return <Main navigate={setIndex}/>;  
+      }
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="w-screen h-screen">
+     {page(index)}
     </div>
   );
+}
+
+const allSites  = ["Bank Page","Facebook", "Twitter", "Apple"]
+
+function Main(props){
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <header className="w-full h-64 bg-black flex flex-col justify-center items-center">
+          <h1 className="text-2xl lg:text-4xl font-bold text-white">All sites built with tailwind</h1>
+          <small className="text-sm text-white">By Augustus Otu</small>
+      </header>
+      <main className="w-full grid grid-cols-2 lg:grid-cols-4 gap-4 p-6">
+            {allSites.map((x,i)=>(<Card key={i} click={()=>props.navigate(allSites.indexOf(x)+1)} site={x}/>))}
+      </main>
+    </div>
+  )
+}
+
+
+function Card(props){
+  return (
+    <div onClick={()=>props.click()} className=" cursor-pointer w-full rounded-lg  h-16 flex flex-row justify-center items-center bg-black">
+        <h2 className="font-bold text-white text-lg">{props.site}</h2>
+    </div>
+  )
 }
---
+++ src/pages/bank.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bank() {
+    return (
+        <div className="w-screen h-screen flex flex-col bg-gradient-to-r from-indigo-900 to-blue-900">
+            
+        </div>
+    )
+}
